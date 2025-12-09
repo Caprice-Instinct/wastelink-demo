d5abb9ab177576036a491a209a3bac425c3a0078
Caprice-Instinct <linetwangui04@gmail.com>
2025-12-09 09:36:56 +0300

    Update video source in Home component to use demo.mp4

--- pages/index.js
+++ pages/index.js
@@ -588,7 +588,7 @@ export default function Home() {
                 controls
                 autoPlay
               >
-                <source src="/WasteLink.mp4" type="video/mp4" />
+                <source src="/demo.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
